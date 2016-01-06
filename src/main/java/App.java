588335e1bd9c0dee3b4a7fa139341d6cd4d959ae
Chris Houghton and Chris Siems <csiems@gmail.com>
2016-01-06 15:36:51 -0800

    add working Spark page.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,46 @@
 import java.util.Random;
 import java.io.Console;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-public class App {
-  public static void main(String[] args) {
-    Console myConsole = System.console();
-    System.out.println("Do you want rock, paper, or scissors?");
-    String user1Input = myConsole.readLine();
-    String user2Input = computerChooses();
-    String winner = checkWinner(user1Input, user2Input);
-    System.out.println("You chose: " + user1Input + ". The computer chose " + user2Input + ". ");
+public class App{
+  public static void main(String[] args){
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/form", (request, response) -> {
+      HashMap model = new HashMap();
+
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+      HashMap model = new HashMap();
+      String user1Input = request.queryParams("user1Input");
+      String user2Input = computerChooses();
+      String winner = checkWinner(user1Input, user2Input);
+
+      model.put("user1Input", user1Input);
+      model.put("user2Input", user2Input);
+      model.put("winner", winner);
+      model.put("template", "templates/output.vtl");
+
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
-
-
 
     public static String computerChooses() {
       String user2Input = "";
-
       Random computerRandomGenerator = new Random();
       Integer computerNumber = computerRandomGenerator.nextInt(3);
-      if (computerNumber < 1) {
+
+      if ( computerNumber < 1 ) {
         user2Input = "rock";
-      } else if(computerNumber > 0 && computerNumber < 2) {
+      } else if( computerNumber > 0 && computerNumber < 2 ) {
         user2Input = "scissors";
       } else {
         user2Input = "paper";
@@ -29,24 +49,24 @@ public class App {
     }
 
     public static String checkWinner(String user1Input, String user2Input) {
-
-       String result = "";
-
+       String winner = "";
        user1Input = user1Input.toLowerCase();
        user1Input = user1Input.replaceAll("//s+", "");
 
-       if( user1Input == "rock" && user2Input == "scissors" ) {
-         result = "Player 1 wins!";
-       } else if( user1Input == "scissors" && user2Input == "paper" ) {
-         result = "Player 1 wins!";
-       } else if ( user1Input == "paper" && user2Input == "rock") {
-         result = "Player 1 wins!";
-       } else if ( user1Input == user2Input) {
-         result = "It's a tie!";
+       if( user1Input.equals("rock") && user2Input.equals("scissors") ) {
+         winner = "You win!";
+       } else if( user1Input.equals("scissors") && user2Input.equals("paper") ) {
+         winner = "You win!";
+       } else if ( user1Input.equals("paper") && user2Input.equals("rock") ) {
+         winner = "You win!";
+       } else if ( user1Input.equals(user2Input) ) {
+         winner = "It's a tie!";
+       } else if( !user1Input.equals("rock") && !user1Input.equals("paper") && !user1Input.equals("scissors") ) {
+         winner = "That is not a valid choice.";
        } else {
-         result = "The computer wins!";
+         winner = "The computer wins!";
        }
-       return result;
+       return winner;
     }
 
 }
