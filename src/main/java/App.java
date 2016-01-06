9b8342236804b5001457414357cc3a2c8584b2a9
Chris Houghton and Chris Siems <csiems@gmail.com>
2016-01-06 11:50:35 -0800

    add random computer choice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,52 @@
+import java.util.Random;
+import java.io.Console;
+
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Console myConsole = System.console();
+    System.out.println("Do you want rock, paper, or scissors?");
+    String user1Input = myConsole.readLine();
+    String user2Input = computerChooses();
+    String winner = checkWinner(user1Input, user2Input);
+    System.out.println("You chose: " + user1Input + ". The computer chose " + user2Input + ". ");
+  }
 
 
-    public Boolean checkWinner(String userInput, String computerResult) {
-       userInput = userInput.toLowerCase();
-       userInput = userInput.replaceAll("//s+", "");
 
-       if (computerResult == "scissors" && userInput == "rock") {
-         return true;
-       }
-       return false;
+    public static String computerChooses() {
+      String user2Input = "";
+
+      Random computerRandomGenerator = new Random();
+      Integer computerNumber = computerRandomGenerator.nextInt(3);
+      if (computerNumber < 1) {
+        user2Input = "rock";
+      } else if(computerNumber > 0 && computerNumber < 2) {
+        user2Input = "scissors";
+      } else {
+        user2Input = "paper";
+      }
+      return user2Input;
     }
+
+    public static String checkWinner(String user1Input, String user2Input) {
+
+       String result = "";
+
+       user1Input = user1Input.toLowerCase();
+       user1Input = user1Input.replaceAll("//s+", "");
+
+       if( user1Input == "rock" && user2Input == "scissors" ) {
+         result = "Player 1 wins!";
+       } else if( user1Input == "scissors" && user2Input == "paper" ) {
+         result = "Player 1 wins!";
+       } else if ( user1Input == "paper" && user2Input == "rock") {
+         result = "Player 1 wins!";
+       } else if ( user1Input == user2Input) {
+         result = "It's a tie!";
+       } else {
+         result = "The computer wins!";
+       }
+       return result;
+    }
+
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,8 +4,26 @@ import static org.junit.Assert.*;
 public class AppTest {
 
   @Test
-  public void checkWinner_rockBeatsScissors_true() {
+  public void checkWinner_rockBeatsScissors_Player1Wins() {
     App appTest = new App();
-    assertEquals(true, appTest.checkWinner("rock", "scissors"));
+    assertEquals("Player 1 wins!", appTest.checkWinner("rock", "scissors"));
   }
+  @Test
+  public void checkWinner_scissorsBeatsPaper_Player1Wins() {
+    App appTest = new App();
+    assertEquals("Player 1 wins!", appTest.checkWinner("scissors", "paper"));
+  }
+  @Test
+  public void checkWinner_paperBeatsRock_Player1Wins() {
+    App appTest = new App();
+    assertEquals("Player 1 wins!", appTest.checkWinner("paper", "rock"));
+  }
+
+  @Test
+  public void computerChooses_shouldBeAString_true() {
+  App appTest = new App();
+  String computerChoice =  appTest.computerChooses();
+  assertEquals(true, computerChoice instanceof String);
+}
+
 }
